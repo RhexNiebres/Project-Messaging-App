a5b6afc7cf36f8b12f9c7274c1b22c33d0ecb463
RhexNiebres <rhexniebres@gmail.com>
2025-04-12 17:48:26 +0800

    refactor: delete unnecessary page for repo restructuring

--- apps/admin/src/services/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-import { isAuthenticated } from "../services/Auth";
-
-const ProtectedRoute = () => {
-  return isAuthenticated() ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default ProtectedRoute;
