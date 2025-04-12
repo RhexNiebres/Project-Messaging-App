be8eff207b0e51d531769092ac1dff35f9387746
RhexNiebres <rhexniebres@gmail.com>
2025-04-12 18:10:27 +0800

    chore: change test text respond by server

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 
 router.get("/", (req, res) => {
-  res.send("Hello, World from server!");
+  res.send("Hello, this is the messenger like app server!");
 });
 
 module.exports = router;
