b9fde386d68f787158109a20414a4b7075f645e0
RhexNiebres <rhexniebres@gmail.com>
2025-04-12 18:08:41 +0800

    feat: update JWT TOKEN Expiration

--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
  
 function generateToken(user){
   return jwt.sign({id: user.id, username: user.username,role:user.role },  process.env.JWT_SECRET,{
-    expiresIn:"24h"
+    expiresIn:"30d"// token expires in 30 days 
   });
 }
 
