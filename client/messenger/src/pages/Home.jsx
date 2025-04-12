e501af7754e32f4213c0dc5dfc9ea353416975fb
RhexNiebres <rhexniebres@gmail.com>
2025-04-12 17:55:47 +0800

    feat: add new React page with initial content

---
+++ client/messenger/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import NavBar from "../components/NavBar";
+
+
+const Home = () => {
+
+  return (
+    <div>
+      <NavBar />
+      <h1>this is the home page</h1>
+    </div>
+  );
+};
+
+export default Home;
