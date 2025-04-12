b652fcdef2e4c8a8556c8548fd004fe7c2ac315c
RhexNiebres <rhexniebres@gmail.com>
2025-04-12 17:57:29 +0800

    feat: add App.jsx with initial content

---
+++ client/messenger/src/App.jsx
@@ -0,0 +1,16 @@
+import '../src/styles/App.css';  
+import NavBar from './components/NavBar';
+import LandingPage from './pages/LandingPage';
+
+const App = () => {
+  return (
+    <div>
+      <NavBar/>
+      <LandingPage/>
+    </div>
+  );
+};
+
+export default App;
+
+
