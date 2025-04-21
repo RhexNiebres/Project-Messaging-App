bfbf8adbcdf1f442b38a4b0f734126144c45eccf
RhexNiebres <rhexniebres@gmail.com>
2025-04-21 16:51:20 +0800

    feat: Set up routes for conversations: GET, POST, DELETE

---
+++ server/src/routes/conversations.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router();
+const conversationsController = require('../controllers/conversationsController');
+
+router.get('/:id', conversationsController.getConversation);
+router.post('/', conversationsController.createConversation);
+router.delete('/:id', conversationsController.deleteConversation);
+
+module.exports = router;
