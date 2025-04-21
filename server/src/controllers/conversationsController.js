b29de95a0248c9d9427226ac0d60078d34f3864f
RhexNiebres <rhexniebres@gmail.com>
2025-04-21 16:49:54 +0800

    feat: add controller for deleting conversations

--- server/src/controllers/conversationsController.js
+++ server/src/controllers/conversationsController.js
@@ -6,11 +6,11 @@ exports.getConversation = async (req, res) => {
   try {
     const conversation = await prisma.conversation.findUnique({
       where: { id: parseInt(id) },
-      include: { message: true, chatMembers: true },
+      include: { messages : true, chatMembers: true },
     });
 
     if (!conversation) {
-      res.status(404).json({ error: "Conversation not found" });
+      res.status(404).json({ message: "Conversation not found" });
     }
 
     res.json(conversation);
@@ -23,16 +23,35 @@ exports.createConversation = async (req, res) => {
   const { chatMembers } = req.body;
   try {
     if (!chatMembers || chatMembers.length < 2) {
-      return res.status(400).json({ error: "at least 2 members are required" });
+      return res.status(400).json({ message: "at least 2 members are required" });
     }
-    const conversation = await prisma.create({
+    const conversation = await prisma.conversation.create({
       data: {
-        chatMembers: { connect: chatMembers.map((userId) => ({ id: userId })), 
-        },
+        chatMembers: { connect: chatMembers.map((userId) => ({ id: userId })) },
       },
     });
-    res.status(200).json(conversation);
-  } catch(error) {
+    res.status(201).json(conversation);
+  } catch (error) {
     res.status(500).json({ error: "Error Creating conversation" });
+  }
+};
+
+exports.deleteConversation = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const conversation = await prisma.conversation.findUnique({
+      where: { id: parseInt(id) },
+    });
+
+    if(!conversation){
+        return res.status(404).json({message: 'Conversation not found'})
+    }
+    await prisma.conversation.delete({
+        where: {id: parseInt(id) },
+    })
+    res.status(200).json({message: 'Conversation deleted successfully'});
+  } catch (error) {
+    res.status(500).json({ error: "Error Deleting conversation" });
   }
 };
