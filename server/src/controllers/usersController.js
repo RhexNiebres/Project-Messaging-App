91c6b067510aa2ffb57b47f2c467ff904f73a4c4
RhexNiebres <rhexniebres@gmail.com>
2025-04-21 15:28:34 +0800

    feat: add controller for getting specific users by its id

--- server/src/controllers/usersController.js
+++ server/src/controllers/usersController.js
@@ -10,3 +10,21 @@ exports.getAllUsers = async(req,res) => {
     }
 };
 
+exports.getUserById = async(req,res) => {
+    const {id} = req.params;
+
+    try{
+        const user= await prisma.findUnique({
+        where: {id:parseInt(id)},
+    });
+
+    if(!user){
+        return res.status(404).json({error: "User not found"})
+    }
+
+    res.status(200).json(user);
+
+    }catch(error){
+        res.status(500).json({error: "Error fetching user"})
+    }
+}
