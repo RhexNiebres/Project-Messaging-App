3e84636daeb20c7eb075abc68c8e61a3558da1f6
RhexNiebres <rhexniebres@gmail.com>
2025-04-21 16:39:22 +0800

    feat: add controller for creating conversations

--- server/src/controllers/conversationsController.js
+++ server/src/controllers/conversationsController.js
@@ -19,3 +19,20 @@ exports.getConversation = async (req, res) => {
   }
 };
 
+exports.createConversation = async (req, res) => {
+  const { chatMembers } = req.body;
+  try {
+    if (!chatMembers || chatMembers.length < 2) {
+      return res.status(400).json({ error: "at least 2 members are required" });
+    }
+    const conversation = await prisma.create({
+      data: {
+        chatMembers: { connect: chatMembers.map((userId) => ({ id: userId })), 
+        },
+      },
+    });
+    res.status(200).json(conversation);
+  } catch(error) {
+    res.status(500).json({ error: "Error Creating conversation" });
+  }
+};
