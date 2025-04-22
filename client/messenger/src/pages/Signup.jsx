9b49f776ef9cfb6d5adbd479d10a2bfba5cedd7d
RhexNiebres <rhexniebres@gmail.com>
2025-04-22 16:15:05 +0800

    feat: add button for login

--- client/messenger/src/pages/Signup.jsx
+++ client/messenger/src/pages/Signup.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { signup } from "../services/Auth";
-
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
+  const navigate = useNavigate();
   const [credentials, setCredentials] = useState({
     username: "",
     email: "",
@@ -35,7 +36,7 @@ const Signup = () => {
   return (
     <div>
       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        <h2 className="text-4xl font-bold text-gray-900 mb-6">Sign Up</h2>
+        <h2 className="text-4xl font-bold text-blue-500 mb-6">Messagely</h2>
 
         {error && <p className="text-red-500 mb-4">{error}</p>}
 
@@ -43,6 +44,7 @@ const Signup = () => {
           onSubmit={handleSignup}
           className="bg-white p-6 rounded-lg shadow-lg w-80"
         >
+           <h2 className="text-2xl font-bold text-gray-900 mb-6">Create a new account</h2> 
           <input
             type="text"
             placeholder="Username"
@@ -84,10 +86,18 @@ const Signup = () => {
           />
           <button
             type="submit"
-            className="w-full bg-blue-600 text-white font-semibold py-3 rounded-md hover:bg-blue-500 transition duration-200"
+            className="w-full bg-blue-500 text-white font-semibold py-3 rounded-md hover:bg-blue-400 transition duration-200"
           >
             Sign Up
           </button>
+          <div className="flex justify-center">
+          <button
+            onClick={() => navigate("/login")}
+            className="bg-green-500 rounded-md p-1 font-semibold text-white mt-5 "
+          >
+            Already have a account?
+          </button>
+          </div>
         </form>
       </div>
     </div>
