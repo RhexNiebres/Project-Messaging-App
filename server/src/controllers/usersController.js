aee8706a94a43e2625d280d1e0b72c798ff7aebc
RhexNiebres <rhexniebres@gmail.com>
2025-04-22 13:19:13 +0800

    feat: add updateUser controller for updating user email and username

--- server/src/controllers/usersController.js
+++ server/src/controllers/usersController.js
@@ -1,30 +1,46 @@
-const { PrismaClient } =require("@prisma/client");
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-exports.getAllUsers = async(req,res) => {
-    try{
-        const users = await prisma.user.findMany();
-        res.status(200).json(users)
-    }catch (error){
-        res.status(500).json({error: "Error fetching users"})
-    }
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching users" });
+  }
 };
 
-exports.getUserById = async(req,res) => {
-    const {id} = req.params;
+exports.getUserById = async (req, res) => {
+  const { id } = req.params;
 
-    try{
-        const user= await prisma.findUnique({
-        where: {id:parseInt(id)},
+  try {
+    const user = await prisma.findUnique({
+      where: { id: parseInt(id) },
     });
 
-    if(!user){
-        return res.status(404).json({error: "User not found"})
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
     }
 
     res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching user" });
+  }
+};
 
-    }catch(error){
-        res.status(500).json({error: "Error fetching user"})
-    }
-}
+exports.updateUser = async (req, res) => {
+
+  const { id } = req.params;
+  const { email, username } = req.body;
+
+  try {
+    const updateUser = await prisma.user.update({
+      where: { id: parseInt(id) },
+      data: { email, username },
+    });
+
+    res.status(200).json(updateUser);
+  } catch (error) {
+    res.status(500).json({ error: "Error updating user information" });
+  }
+};
