eaecdfd5922133d5e8ef0fc887bb5b10485560e1
RhexNiebres <rhexniebres@gmail.com>
2025-04-22 18:04:15 +0800

    chore: add initial page content

---
+++ client/messenger/src/pages/UserProfile.jsx
@@ -0,0 +1,10 @@
+import NavBar from "../components/NavBar";
+const UserProfile = () => {
+    return(
+        <div>
+           <NavBar/>
+            this is profile 
+        </div>
+    );
+};
+export default UserProfile;
