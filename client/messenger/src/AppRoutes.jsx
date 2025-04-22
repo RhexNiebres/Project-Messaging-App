01d1811e13ff80e8c6eb5ccc35c65afca2e1931b
RhexNiebres <rhexniebres@gmail.com>
2025-04-22 18:02:53 +0800

    feat: implement protectedRoute service for route security

--- client/messenger/src/AppRoutes.jsx
+++ client/messenger/src/AppRoutes.jsx
@@ -3,17 +3,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
+import UserProfile from "./pages/UserProfile";
+import ProtectedRoute from "./services/ProtectedRoute";
 import "../src/styles/App.css";
 import App from "./App";
 
 const AppRoutes = () => {
   return (
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/login" element={<Login role="user" />} />
-        <Route path="/signup" element={<Signup role="user" />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<Login role="user" />} />
+      <Route path="/signup" element={<Signup role="user" />} />
+      <Route element={<ProtectedRoute />}>
+      <Route path="/home" element={<Home />} />
+      <Route path="/profile" element={<UserProfile />} />
+      </Route>
+    </Routes>
   );
 };
 
