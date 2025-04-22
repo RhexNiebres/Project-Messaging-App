6e8fc92d0c73ec621bca7277c4d34018b7f6c97e
RhexNiebres <rhexniebres@gmail.com>
2025-04-22 13:16:33 +0800

    feat: add rote for updating user

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const usersController = require('../controllers/usersController')
+const usersController = require("../controllers/usersController");
 
-router.get('/', usersController.getAllUsers)
-router.get('/:id', usersController.getUserById)
+router.get("/", usersController.getAllUsers);
+router.get("/:id", usersController.getUserById);
+router.get("/:id", usersController.updateUser);
 
 module.exports = router;
