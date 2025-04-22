fea2f3b4446634b95767ce127844a6e2acc787f3
RhexNiebres <rhexniebres@gmail.com>
2025-04-22 15:13:05 +0800

    chore: update PrismaClient import to use generated path

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,13 +1,12 @@
 const bcrypt = require("bcryptjs");
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient } = require("../generated/prisma");
 const prisma = new PrismaClient();
-const { generateToken } = require("../middlewares/auth"); 
-require("dotenv").config({path:"../../.env"});
-
+const { generateToken } = require("../middlewares/auth");
+require("dotenv").config({ path: "../../.env" });
 
 exports.postSignUp = async (req, res, next) => {
   try {
-    const { username, email, password, } = req.body;
+    const { username, email, password } = req.body;
 
     if (!username || !password || !email) {
       return res.status(400).json({ message: "All fields are required." });
@@ -18,7 +17,8 @@ exports.postSignUp = async (req, res, next) => {
     });
 
     if (existingUser) {
-      return res.status(400).json({ message: "Username already exists." });    }
+      return res.status(400).json({ message: "Username already exists." });
+    }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
@@ -32,15 +32,15 @@ exports.postSignUp = async (req, res, next) => {
 
     const token = generateToken(newUser);
 
-    res.status(201).json({message:"User registered successfully", token})
+    res.status(201).json({ message: "User registered successfully", token });
   } catch (err) {
     return next(err);
   }
 };
 
-exports.postLogin = async(req,res, next) =>{
-  try{
-    const {username, password}= req.body;
+exports.postLogin = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
 
     const user = await prisma.user.findUnique({
       where: { username },
@@ -52,20 +52,21 @@ exports.postLogin = async(req,res, next) =>{
       },
     });
 
-
-    if(!user) return res.status(400).json({ message: "Invalid username or password." });
+    if (!user)
+      return res.status(400).json({ message: "Invalid username or password." });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if(!isMatch) return res.status(400).json({ message: "Invalid username or password." });
-      
-      const token = generateToken(user);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid username or password." });
 
-      res.json({message: "Login successful", token, role: user.role, })
-  }catch(err){
+    const token = generateToken(user);
+
+    res.json({ message: "Login successful", token, role: user.role });
+  } catch (err) {
     next(err);
   }
 };
 
 exports.logout = (req, res) => {
- res.json({message:"Logout successful"})
+  res.json({ message: "Logout successful" });
 };
