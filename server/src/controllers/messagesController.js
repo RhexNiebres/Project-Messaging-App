6bd8876177e8fbe4c12bdeab9af891e3b9d78a91
RhexNiebres <rhexniebres@gmail.com>
2025-04-22 12:34:25 +0800

    feat: add sendMessages for sending  messages on a  specific conversation

--- server/src/controllers/messagesController.js
+++ server/src/controllers/messagesController.js
@@ -13,9 +13,36 @@ exports.getMessageByConversation = async (req, res) => {
     if (!conversation) {
       res.status(404).json({ message: "Conversation not found" });
     }
-    
+
     res.json(conversation.messages);
   } catch (error) {
     res.status(500).json({ error: "Error fetching messages" });
+  }
+};
+
+exports.sendMessage = async (req, res) => {
+  const { id } = req.params;
+  const { content, senderId } = req.body;
+
+  try {
+    const conversation = await prisma.conversation.findUnique({
+      where: { id: parseInt(id) },
+    });
+
+    if (!conversation) {
+      res.status(404).json({ message: "Conversation not found" });
+    }
+
+    const message = await prisma.message.create({
+      data: {
+        content,
+        senderId,
+        conversationId: parseInt(id),
+      },
+    });
+
+    res.status (201).json(message);
+  } catch (error) {
+    res.status(500).json({ error: "Error sending message" });
   }
 };
