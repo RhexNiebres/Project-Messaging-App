725ec179ec87285a309b7171430971f4ef333630
RhexNiebres <rhexniebres@gmail.com>
2025-04-22 13:18:06 +0800

    chore: fix formatting using prettier

--- server/src/routes/conversations.js
+++ server/src/routes/conversations.js
@@ -1,9 +1,9 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const conversationsController = require('../controllers/conversationsController');
+const conversationsController = require("../controllers/conversationsController");
 
-router.get('/:id', conversationsController.getConversation);
-router.post('/', conversationsController.createConversation);
-router.delete('/:id', conversationsController.deleteConversation);
+router.get("/:id", conversationsController.getConversation);
+router.post("/", conversationsController.createConversation);
+router.delete("/:id", conversationsController.deleteConversation);
 
 module.exports = router;
