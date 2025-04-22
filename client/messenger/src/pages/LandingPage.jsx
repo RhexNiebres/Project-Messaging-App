aa984cdc8e56acf7138f489e38d2dc43b58e94fc
RhexNiebres <rhexniebres@gmail.com>
2025-04-22 18:08:09 +0800

    feat: import login page

--- client/messenger/src/pages/LandingPage.jsx
+++ client/messenger/src/pages/LandingPage.jsx
@@ -1,3 +1,4 @@
+import Login from "./Login";
 const LandingPage = () => {
   return (
     <div className="flex justify-center items-center bg-gray-100 min-h-screen gap-10">
@@ -5,8 +6,8 @@ const LandingPage = () => {
     <h1 className="text-blue-500 font-extrabold text-7xl mb-4">Messagely</h1>
     <p className="text-4xl max-w-md">Connect with friends and stay in touch with the world on Messagely.</p>
       </section>
-      <div className="bg-blue-500 p-4 text-white">
-    <h1 ></h1>
+      <div>
+        <Login/>
       </div>
     </div>
   );
