63009e1bfbd2eb691baa4a269651aa8c96fc3a8e
RhexNiebres <rhexniebres@gmail.com>
2025-04-22 15:08:19 +0800

    chore: import usersRoutes in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const indexRoutes = require("./routes/index");
 const authRoutes = require("./routes/auth");
+const usersRoutes = require("./routes/users")
 const cors = require("cors");
 
 app.use(cors({ origin: "*", credentials: true }));
@@ -11,10 +12,10 @@ app.options("*", cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/comments", commentRoutes);
-app.use("/posts", postRoutes);
 app.use("/", indexRoutes);
 app.use("/", authRoutes);
+app.use("/users", usersRoutes);
+
 
 app.listen(process.env.APP_PORT, () =>
   console.log(`App listening on port ${process.env.APP_PORT}!`)
