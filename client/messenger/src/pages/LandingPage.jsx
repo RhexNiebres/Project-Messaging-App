f8d11e5c7d43d0c20db9934d78f75aa7b1435ac3
RhexNiebres <rhexniebres@gmail.com>
2025-04-22 16:51:02 +0800

    chore: set up initial content

--- client/messenger/src/pages/LandingPage.jsx
+++ client/messenger/src/pages/LandingPage.jsx
@@ -1,9 +1,15 @@
-const LandingPage = () =>{
-    return(
-        <div>
-            this is the landing page
-        </div>
-    );
-}
+const LandingPage = () => {
+  return (
+    <div className="flex justify-center items-center bg-gray-100 min-h-screen gap-10">
+      <section>
+    <h1 className="text-blue-500 font-extrabold text-7xl mb-4">Messagely</h1>
+    <p className="text-4xl max-w-md">Connect with friends and stay in touch with the world on Messagely.</p>
+      </section>
+      <div className="bg-blue-500 p-4 text-white">
+    <h1 ></h1>
+      </div>
+    </div>
+  );
+};
 
 export default LandingPage;
