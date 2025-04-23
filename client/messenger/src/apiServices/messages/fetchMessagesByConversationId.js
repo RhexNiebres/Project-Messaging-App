f7c62ff530ad006b4dd50daf6c7431b693fdefe7
RhexNiebres <rhexniebres@gmail.com>
2025-04-23 17:22:22 +0800

    feat: add auth headers to API requests

--- client/messenger/src/apiServices/messages/fetchMessagesByConversationId.js
+++ client/messenger/src/apiServices/messages/fetchMessagesByConversationId.js
@@ -1,17 +1,22 @@
 export const fetchMessagesByConversationId = async (conversationId) => {
-    try {
-      const response = await fetch(
-        import.meta.env.VITE_HOST + `/${conversationId}/messages`
-      );
-  
-      if (!response.ok) {
-        throw new Error("Failed to fetch messages");
+  try {
+    const response = await fetch(
+      import.meta.env.VITE_HOST + `/${conversationId}/messages`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
       }
-  
-      const data = await response.json();
-      return { success: true, messages: data };
-    } catch (error) {
-      return { success: false, error: error.message };
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch messages");
     }
-  };
-  
+
+    const data = await response.json();
+    return { success: true, messages: data };
+  } catch (error) {
+    return { success: false, error: error.message };
+  }
+};
