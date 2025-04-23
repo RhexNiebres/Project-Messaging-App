678a822f84b0f1b04c5fdd4ffd962dc84718c038
RhexNiebres <rhexniebres@gmail.com>
2025-04-23 12:29:17 +0800

    feat: add sendMessage function to handle adding messages via API

---
+++ client/messenger/src/apiServices/messages/sendMessage.js
@@ -0,0 +1,24 @@
+export const sendMessage = async (conversationId, content, senderId) => {
+    try {
+      const response = await fetch(
+        import.meta.env.VITE_HOST + `/${conversationId}/messages`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ content, senderId }),
+        }
+      );
+  
+      if (!response.ok) {
+        throw new Error("Failed to send message");
+      }
+  
+      const data = await response.json();
+      return { success: true, message: data };
+    } catch (error) {
+      return { success: false, error: error.message };
+    }
+  };
+  
