2e7278b4f06228c856eabbd9684934132985bb9f
RhexNiebres <rhexniebres@gmail.com>
2025-04-23 12:00:47 +0800

    feat: add deleteMessageById function to handle message deletion via API

---
+++ client/messenger/src/apiServices/messages/deleteMessageById.js
@@ -0,0 +1,20 @@
+export const deleteMessageById = async (messageId) => {
+    try {
+      const response = await fetch(
+        import.meta.env.VITE_HOST + `/messages/${messageId}`,
+        {
+          method: "DELETE",
+        }
+      );
+  
+      if (!response.ok) {
+        throw new Error("Failed to delete message");
+      }
+  
+      const data = await response.json();
+      return { success: true, message: data.message };
+    } catch (error) {
+      return { success: false, error: error.message };
+    }
+  };
+  
