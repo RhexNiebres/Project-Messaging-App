349dfbdc7afb246857991f674a279c14512ec4d7
RhexNiebres <rhexniebres@gmail.com>
2025-04-23 12:01:26 +0800

    feat: add fetchMessageByConversationId function to handle fetch messages in a conversation via API

---
+++ client/messenger/src/apiServices/messages/fetchMessagesByConversationId.js
@@ -0,0 +1,17 @@
+export const fetchMessagesByConversationId = async (conversationId) => {
+    try {
+      const response = await fetch(
+        import.meta.env.VITE_HOST + `/${conversationId}/messages`
+      );
+  
+      if (!response.ok) {
+        throw new Error("Failed to fetch messages");
+      }
+  
+      const data = await response.json();
+      return { success: true, messages: data };
+    } catch (error) {
+      return { success: false, error: error.message };
+    }
+  };
+  
