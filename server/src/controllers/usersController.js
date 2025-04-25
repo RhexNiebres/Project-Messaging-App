e4936bb4f5618901aee86395c0118d03261f17e1
RhexNiebres <rhexniebres@gmail.com>
2025-04-25 16:22:28 +0800

    feat: add  gender and password on controller to be able to update  gender and password

--- server/src/controllers/usersController.js
+++ server/src/controllers/usersController.js
@@ -1,5 +1,6 @@
 const { PrismaClient } = require("../generated/prisma");
 const prisma = new PrismaClient();
+const bcrypt = require("bcryptjs");
 
 exports.getAllUsers = async (req, res) => {
   try {
@@ -30,19 +31,36 @@ exports.getUserById = async (req, res) => {
 
 exports.updateUser = async (req, res) => {
   const { id } = req.params;
-  const { email, username } = req.body;
+  const { email, username, password, gender } = req.body;
 
   try {
-    const updateUser = await prisma.user.update({
+  
+    const updateData = {};
+
+    if (email)    updateData.email    = email;
+    if (username) updateData.username = username;
+    if (gender)   updateData.gender   = gender;
+
+    if (password) {
+      const hashed = await bcrypt.hash(password, 10);
+      updateData.password = hashed;
+    }
+
+    const updatedUser = await prisma.user.update({
       where: { id: parseInt(id) },
-      data: { email, username },
+      data: updateData,
     });
 
-    res.status(200).json(updateUser);
+    // to not return the hashed password
+    const { password: _pw, ...safeUser } = updatedUser;
+
+    res.status(200).json(safeUser);
   } catch (error) {
+    console.error("Update error:", error);
     res.status(500).json({ error: "Error updating user information" });
   }
 };
+
 
 exports.checkUserExistence = async (req, res) => {
   const { username, email, userId } = req.body;
