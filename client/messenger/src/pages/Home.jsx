23d8356bcabf08d3a2d0a5b846199949ba54ce6f
RhexNiebres <rhexniebres@gmail.com>
2025-04-25 13:01:56 +0800

    chore: import NavBar and AddConversation component

--- client/messenger/src/pages/Home.jsx
+++ client/messenger/src/pages/Home.jsx
@@ -1,13 +1,25 @@
+import React from "react";
 import NavBar from "../components/NavBar";
-
+import ConversationList from "../components/ConversationList";
+import AddConversation from "../components/AddConversation";
 
 const Home = () => {
+  const currentUserId = localStorage.getItem("userId");
 
   return (
-    <div>
+    <>
       <NavBar />
-      <h1>this is the home page</h1>
-    </div>
+      <div className="mt-20 bg-blue-500 p-7  flex flex-col justify-start gap-10 w-80 rounded-2xl mx-2">
+        {/*Conversation Component */}
+        <AddConversation currentUserId={currentUserId} />
+
+        {/* Conversation List Component */}
+        <ConversationList currentUserId={currentUserId} />
+
+        {/* add messageBoard Component  if component is finished */}
+        
+      </div>
+    </>
   );
 };
 
