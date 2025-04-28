228394d8233dce192803e1407709809a6c635134
RhexNiebres <rhexniebres@gmail.com>
2025-04-28 16:43:46 +0800

    chore: remove role on jwt sign token

--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -2,7 +2,7 @@ require("dotenv").config({path:"../../.env"});
 const jwt = require("jsonwebtoken");
  
 function generateToken(user){
-  return jwt.sign({id: user.id, username: user.username,role:user.role },  process.env.JWT_SECRET,{
+  return jwt.sign({id: user.id, username: user.username,},  process.env.JWT_SECRET,{
     expiresIn:"30d"// token expires in 30 days 
   });
 }
