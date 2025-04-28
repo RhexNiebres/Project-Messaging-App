434c0385ebb8c2584cf1c8345de8cd12fd9b8f70
RhexNiebres <rhexniebres@gmail.com>
2025-04-28 16:51:03 +0800

    feat: add Token verification on messages routes

--- server/src/routes/messages.js
+++ server/src/routes/messages.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const messageController = require("../controllers/messagesController");
+const { verifyToken } = require("../middlewares/auth");
 
-router.get("/:id/messages", messageController.getMessageByConversation);
-router.post("/:id/messages", messageController.sendMessage);
-router.delete("/:id", messageController.deleteMessage);
+router.get("/:id/messages", verifyToken, messageController.getMessageByConversation);
+router.post("/:id/messages", verifyToken, messageController.sendMessage);
+router.delete("/:id", verifyToken, messageController.deleteMessage);
 
-module.exports = router;
+module.exports = router; 
