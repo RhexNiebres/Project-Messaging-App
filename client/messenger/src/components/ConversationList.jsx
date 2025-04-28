91bcaef6e7f4c2162042a8f103ccb361b52d5215
RhexNiebres <rhexniebres@gmail.com>
2025-04-28 16:27:51 +0800

    feat: add setExistingConversation state for displaying existing conversations and delete old useNavigate

--- client/messenger/src/components/ConversationList.jsx
+++ client/messenger/src/components/ConversationList.jsx
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { fetchConversations } from "../apiServices/conversations/fetchConversations";
-import { useNavigate } from "react-router-dom";
 
-const ConversationList = ({ currentUserId }) => {
-  const navigate = useNavigate();
-
+const ConversationList = ({ currentUserId, setExistingConversation }) => {
   const [userConversations, setUserConversations] = useState([]);
   const [errorMsg, setErrorMsg] = useState("");
 
@@ -29,8 +26,10 @@ const ConversationList = ({ currentUserId }) => {
   }, [currentUserId]);
 
   return (
-    <div className="p-4 max-w-md mx-auto bg-white shadow-md rounded-md">
-      <h2 className="text-lg font-semibold mb-4 text-center text-white bg-blue-500 p-2 rounded-2xl">Conversations</h2>
+    <div className="p-4 max-w-80 h-2/3 mx-5 bg-white shadow-md rounded-md">
+      <h2 className="text-lg font-semibold mb-4 text-center text-white bg-blue-500 p-2 rounded-2xl">
+       Select Conversation
+      </h2>
 
       {errorMsg && <div className="text-red-500 mb-2">{errorMsg}</div>}
 
@@ -40,7 +39,7 @@ const ConversationList = ({ currentUserId }) => {
             <li
               key={convo.id}
               className="border p-2 rounded mb-4 cursor-pointer hover:bg-gray-100"
-              onClick={() => navigate(`/conversation/${convo.id}`)}
+              onClick={() => setExistingConversation(convo)}
             >
               <p className="font-medium text-blue-500">
                 Members: {convo.chatMembers.map((m) => m.username).join(", ")}
