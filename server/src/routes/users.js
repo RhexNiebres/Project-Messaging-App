6bea8a55b87e5cdd668adebf155d7e146df6da8d
RhexNiebres <rhexniebres@gmail.com>
2025-04-28 16:51:17 +0800

    feat: add Token verification on users routes

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/usersController");
+const conversationsController = require("../controllers/conversationsController");
+const { verifyToken } = require("../middlewares/auth");
 
-router.get("/", usersController.getAllUsers);
-router.get("/:id", usersController.getUserById);
-router.put("/:id", usersController.updateUser);
+router.get("/", verifyToken, usersController.getAllUsers);
+router.get("/:id", verifyToken, usersController.getUserById);
+router.put("/:id", verifyToken, usersController.updateUser);
+router.get(
+  "/:id/conversations",
+  verifyToken,
+  conversationsController.getUserConversations
+);
+router.post('/check', verifyToken,
+    usersController.checkUserExistence);
 
 module.exports = router;
