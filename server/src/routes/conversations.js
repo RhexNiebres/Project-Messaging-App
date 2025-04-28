70071c19f6cd7aedbd2ee90b01a7a73be641d791
RhexNiebres <rhexniebres@gmail.com>
2025-04-28 16:50:45 +0800

    feat: add Token verification on conversations routes

--- server/src/routes/conversations.js
+++ server/src/routes/conversations.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const conversationsController = require("../controllers/conversationsController");
+const { verifyToken } = require("../middlewares/auth");
 
-router.get("/:id", conversationsController.getConversation);
-router.post("/", conversationsController.createConversation);
-router.delete("/:id", conversationsController.deleteConversation);
+router.get("/:id", verifyToken, conversationsController.getConversation);
+router.post("/", verifyToken, conversationsController.createConversation);
+router.delete("/:id", verifyToken, conversationsController.deleteConversation);
 
 module.exports = router;
