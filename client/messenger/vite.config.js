e627f77171d22fd842686804c2b222b298e69429
RhexNiebres <rhexniebres@gmail.com>
2025-04-29 11:16:39 +0800

    chore: update  contents

--- client/messenger/vite.config.js
+++ client/messenger/vite.config.js
@@ -3,16 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/",
   server: {
     proxy: {
       "/auth": process.env.HOST,
-    },
-  },
-
-  resolve: {
-    alias: {
-      "@shared": "/apps/shared",
     },
   },
 });
