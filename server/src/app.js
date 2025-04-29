e87461d365390478007ddeb2375c96ffa036203a
RhexNiebres <rhexniebres@gmail.com>
2025-04-29 20:57:04 +0800

    feat: update cors to allow OPTIONS method

--- server/src/app.js
+++ server/src/app.js
@@ -13,9 +13,12 @@ const conversationsRoutes = require("./routes/conversations");
 const messagesRoutes = require("./routes/messages");
 const cors = require("cors");
 
-app.use(cors({ origin: "*", credentials: true }));
-app.options("*", cors());
-
+app.use(cors({
+  origin: process.env.MESSAGELY_CLIENT_HOST, //Netlify URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
+app.options('*', cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
