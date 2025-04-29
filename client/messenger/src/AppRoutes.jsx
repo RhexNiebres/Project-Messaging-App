5c47113b41bb838fc8ec19d13f0f50fc0855290f
RhexNiebres <rhexniebres@gmail.com>
2025-04-29 11:09:19 +0800

    chore: format using prettier

--- client/messenger/src/AppRoutes.jsx
+++ client/messenger/src/AppRoutes.jsx
@@ -16,8 +16,8 @@ const AppRoutes = () => {
       <Route path="/" element={<App />} />
       <Route index element={<LandingPage />} />
 
-      <Route path="/login" element={<Login role="user" />} />
-      <Route path="/signup" element={<Signup role="user" />} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/signup" element={<Signup/>} />
       <Route element={<ProtectedRoute />}>
         <Route path="/home" element={<Home />} />
         <Route path="/profile" element={<UserProfile />} />
