fa1151ceddde62896c4b7ad2e38cdc85b1ad2954
RhexNiebres <rhexniebres@gmail.com>
2025-04-29 10:36:27 +0800

    chore: format using prettier

--- client/messenger/src/pages/LandingPage.jsx
+++ client/messenger/src/pages/LandingPage.jsx
@@ -3,11 +3,15 @@ const LandingPage = () => {
   return (
     <div className="flex justify-center items-center bg-gray-100 min-h-screen gap-10">
       <section>
-    <h1 className="text-blue-500 font-extrabold text-7xl mb-4">Messagely</h1>
-    <p className="text-4xl max-w-md">Connect with friends and stay in touch with the world on Messagely.</p>
+        <h1 className="text-blue-500 font-extrabold text-7xl mb-4">
+          Messagely
+        </h1>
+        <p className="text-4xl max-w-md">
+          Connect with friends and stay in touch with the world on Messagely.
+        </p>
       </section>
       <div>
-        <Login/>
+        <Login />
       </div>
     </div>
   );
