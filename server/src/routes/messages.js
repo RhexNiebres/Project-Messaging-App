1ef1045e9deb02926f3c5b1917b7fc644f3b4145
RhexNiebres <rhexniebres@gmail.com>
2025-05-02 13:07:26 +0800

    chore: removed unused route

--- server/src/routes/messages.js
+++ server/src/routes/messages.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const messageController = require("../controllers/messagesController");
 const { verifyToken } = require("../middlewares/auth");
 
-router.get("/:id/messages", verifyToken, messageController.getMessageByConversation);
 router.post("/:id/messages", verifyToken, messageController.sendMessage);
 router.delete("/:id", verifyToken, messageController.deleteMessage);
 
