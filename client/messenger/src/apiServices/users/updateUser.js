e16bc6b7ebc8abb7549794a14bc7cbf1e658ff5d
RhexNiebres <rhexniebres@gmail.com>
2025-05-02 10:17:33 +0800

    refactor: modify fetch request payload structure

--- client/messenger/src/apiServices/users/updateUser.js
+++ client/messenger/src/apiServices/users/updateUser.js
@@ -1,27 +1,25 @@
 export const updateUser = async (id, userData) => {
-    try {
-      const response = await fetch(import.meta.env.VITE_HOST + `/users/${id}`,{
-        method:"PUT",
-        headers:{
-            "Content-Type":"application/json",
-            'Authorization': `Bearer ${localStorage.getItem('token')}`,
-        },
-        body:JSON.stringify(userData)
-      });
-        
-      if (!response.ok) {
-        throw new Error("Failed to update user information");
-      }
-  
-      const data = await response.json();
-  
-      if (data) {
-        return { success: true, user: data };
-      } else {
-        throw new Error("Failed to update user.");
-      }
-    } catch (error) {
-      return { success: false, error: error.message };
+  try {
+    const response = await fetch(import.meta.env.VITE_HOST + `/users/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      body: JSON.stringify(userData),
+    });
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.error || "Failed to update user information");
     }
-  };
-  
+
+    if (data) {
+      return { success: true, user: data };
+    } else {
+      throw new Error("Failed to update user.");
+    }
+  } catch (error) {
+    return { success: false, error: error.message };
+  }
+};
