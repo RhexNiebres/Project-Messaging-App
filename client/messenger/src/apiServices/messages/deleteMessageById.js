a0d239159f08052b421d1f4c4745d552e544fd81
RhexNiebres <rhexniebres@gmail.com>
2025-05-02 12:16:28 +0800

    chore: format using prettier

--- client/messenger/src/apiServices/messages/deleteMessageById.js
+++ client/messenger/src/apiServices/messages/deleteMessageById.js
@@ -1,23 +1,26 @@
 export const deleteMessageById = async (messageId) => {
-    try {
-      const response = await fetch(
-        import.meta.env.VITE_HOST + `/messages/${messageId}`,
-        {
-          method: "DELETE",
-          headers: {
-            'Authorization': `Bearer ${localStorage.getItem('token')}`, 
-          },
-        }
-      );
-  
-      if (!response.ok) {
-        throw new Error("Failed to delete message");
+  try {
+    const response = await fetch(
+      import.meta.env.VITE_HOST + `/messages/${messageId}`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
       }
-  
-      const data = await response.json();
-      return { success: true, message: data.message, deletedMessage: data.deletedMessage};
-    } catch (error) {
-      return { success: false, error: error.message };
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to delete message");
     }
-  };
-  
+
+    const data = await response.json();
+    return {
+      success: true,
+      message: data.message,
+      deletedMessage: data.deletedMessage,
+    };
+  } catch (error) {
+    return { success: false, error: error.message };
+  }
+};
