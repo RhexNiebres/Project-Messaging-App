58683d61cfab5ab69750a2aa3646d8b19dc6785b
RhexNiebres <rhexniebres@gmail.com>
2025-05-02 13:07:13 +0800

    chore: removed unused route

--- server/src/routes/conversations.js
+++ server/src/routes/conversations.js
@@ -5,7 +5,10 @@ const { verifyToken } = require("../middlewares/auth");
 
 router.get("/:id", verifyToken, conversationsController.getConversation);
 router.post("/", verifyToken, conversationsController.createConversation);
-router.delete("/:id", verifyToken, conversationsController.deleteConversation);
-router.get('/:id/conversations', verifyToken, conversationsController.getUserConversations);
+router.get(
+  "/:id/conversations",
+  verifyToken,
+  conversationsController.getUserConversations
+);
 
 module.exports = router;
